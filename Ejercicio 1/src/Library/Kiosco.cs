96531d482ac6ef7e371d8dc83232b6ed387a8b2f
Gonzalo Lopez <gonzalopez4@gmail.com>
2020-09-01 20:19:06 -0300

    Add files via upload

---
+++ Ejercicio 1/src/Library/Kiosco.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Expert_SRP
+{
+    public class Kiosco 
+    {
+        public Boolean PuedeComprar(Alfajor a, Double dinero, String moneda) 
+        {
+            Double pesos = ConvertirAPesos(dinero, moneda);
+            return pesos >= a.PrecioDulce + a.PrecioMasa;
+        }
+    }
+}

073376e56c7de1350169d16bc58d25c7196cd568
Gonzalo Lopez <gonzalopez4@gmail.com>
2020-09-04 18:54:12 -0300

    Update Kiosco.cs

--- Ejercicio 1/src/Library/Kiosco.cs
+++ Ejercicio 1/src/Library/Kiosco.cs
@@ -2,7 +2,17 @@ using System;
 
 namespace Expert_SRP
 {
-    public class Kiosco 
+    /*
+    SRP: 
+    Kiosco no cumple con el patron SRP debido a que tiene dos responsabilidades (o dos razones de cambio) asociadas a esta misma clase, 
+    verificar si se puede comprar y hacer el cambio de moneda. Las responsabilidades asignadas no estan relacionadas con solo una funcionalidad.
+    Por ejemplo el tipo de cambio podria cambiar a dolares y esto se vería afectado. Por lo que habria que independizar el tipo de cambio a una nueva clase o alfajor.
+
+    EXPERT:
+    Kiosco es el responsable de calcular el costo del alfajor, y estos datos se los pedia a Alfajor por lo cual no cumple el patron Expert
+    debido a que no se le otorga la informacion al experto en la informacion, en este caso Alfajor era el experto en esa clase.
+    
+    */
     {
         public Boolean PuedeComprar(Alfajor a, Double dinero, String moneda) 
         {

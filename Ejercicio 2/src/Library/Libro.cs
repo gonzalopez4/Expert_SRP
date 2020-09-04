8d7e94ef1fc7ebe4f09949c856fd3bd564cb8a55
Gonzalo Lopez <gonzalopez4@gmail.com>
2020-09-04 18:49:25 -0300

    Add files via upload

---
+++ Ejercicio 2/src/Library/Libro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SRP
+{
+    /*
+    La clase libro no cumple con SRP pues tiene dos razones de cambio, la ubicacion y la informacion.
+    El libro no tiene por qué saber donde esta colocado en la biblioteca.
+    Se deberia crear una nueva clase encargada de tener la responabilidad de almacenar el libro 
+    en un estante y otra nueva que almacene los estantes
+    */
+    public class Libro
+    {
+
+        public string Titulo { get ; }
+        public string Autor { get ; }
+        public string Codigo { get ;  }
+        public string SectorBiblioteca { get ; set; }
+        public string EstanteBiblioteca { get ; set; }
+
+        public Libro(String titulo, String autor, String codigo)
+        {
+            this.Titulo = titulo;
+            this.Autor = autor;
+            this.Codigo = codigo;
+        }
+
+        public void AlmacenarLibro(String sector, String estante)
+        {
+            this.SectorBiblioteca = sector;
+            this.EstanteBiblioteca = estante;
+        }
+
+    }
+}
